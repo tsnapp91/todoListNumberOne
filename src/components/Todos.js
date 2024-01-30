a2013a37683cc1131978ba7fa6df77ab9c7cf40c
Travis Snapp <traviswsnapp@outlook.com>
2024-01-29 20:06:04 -0500

    redux connected to todos component

---
+++ src/components/Todos.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { addTodos } from "../redux/reducer";
+
+const mapStateToProps = (state) => {
+  return {
+    todos: state,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addTodo: (obj) => dispatch(addTodos(obj)),
+  };
+};
+
+const Todos = (props) => {
+  const [todo, setTodo] = useState("");
+
+  const handleChange = (e) => {
+    setTodo(e.target.value);
+  };
+
+  console.log("props from store", props);
+
+  return (
+    <div className="addTodos">
+      <input
+        type="text"
+        onChange={(e) => handleChange(e)}
+        className="todo-input"
+      />
+
+      <button
+        className="add-btn"
+        onClick={() => {
+          props.addTodo({
+            id: Math.floor(Math.random() * 1000),
+            item: todo,
+            completed: false,
+          });
+        }}
+      >
+        Add
+      </button>
+      <br />
+
+      <ul>
+        {props.todos.map((item) => {
+          return <li key={item.id}>{item.item}</li>;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Todos);
